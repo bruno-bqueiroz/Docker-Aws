990bcdb0a6272a79c50569f2f0925be97adee6ee
BRUNO BARBOSA <brunobdq32@gmail.com>
2023-01-06 09:37:30 -0400

    feat: filtrgaem por dia do envento pronta no back-end

--- src/controllers/activities-controller.ts
+++ src/controllers/activities-controller.ts
@@ -4,7 +4,7 @@ import httpStatus from "http-status";
 import activitiesService from "@/services/activities-service";
 
 export async function getActivities(req: AuthenticatedRequest, res: Response) {
-  const day = "2023-10-22";
+  const { day } = req.body;
 
   if(!day) return res.sendStatus(httpStatus.BAD_REQUEST);
 
